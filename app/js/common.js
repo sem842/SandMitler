f8f3ae2b9097bbc0bea16f5ff7423139ad634eb4
Semen Khizhnyak <gsem842@gmail.com>
2017-05-03 17:23:00 +0700

    17.05.03 carousel proccess

--- app/js/common.js
+++ app/js/common.js
@@ -8,19 +8,41 @@ $(function() {
 		offCanvas: {
 			position: 'right'
 		}
-
 	});
 
-	console.log($("#my-menu"));
-
-	var api = $("#my-menu").data("mmenu");
+	var api	= $("#my-menu").data("mmenu");
 	var hamburg = $(".hamburger");
-	api.bind("open:start",function() {
+	api.bind("open:finish",function() {
 		hamburg.addClass("is-active")		
 	}).bind('close:finish',function() {
 		hamburg.removeClass('is-active')		
 	});
 
+	$('.carousel-services').owlCarousel({
+		loop: true,
+		nav: true,
+		smartSpeed: 700,
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-left"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			800: {
+				items: 2
+			},
+			1100: {
+				items: 3
+			}
+		}//end responsive parametrs
+	});//end owl carousel
 
-
-});
+	function carouselService() {
+		$('.carousel-services-item').each(function() {
+			var ths  = $(this),
+					thsh = ths.find('.carousel-services-content').outerHeight();
+					ths.find('.carousel-services-image').css('min-height', thsh);
+		});//end each function
+	}//end carouselService function
+	carouselService();
+});//end main function
